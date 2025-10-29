61e58fabd1fea0d101ababafd9b65c4f3399f8a4
Jason Gibson <76972956+fingergunsDEV@users.noreply.github.com>
2025-10-29 09:54:19 -0500

    Create game.ts

---
+++ client/src/game.ts
@@ -0,0 +1,6 @@
+import BootScene from './scenes/BootScene';
+import PreloadScene from './scenes/PreloadScene';
+import LoginScene from './scenes/LoginScene';
+import WorldScene from './scenes/WorldScene';
+
+export default [BootScene, PreloadScene, LoginScene, WorldScene];
